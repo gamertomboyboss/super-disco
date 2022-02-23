3f88deb8dcdd2cea13cc15d78b7a01bd901406ab
gamertomboyboss <78607496+gamertomboyboss@users.noreply.github.com>
2022-02-22 19:40:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+video = "";
+objects  = [];
+
+function setup()
+{
+    canvas = createCanvas(380,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+function draw()
+{
+    image(video, 0, 0, 380, 380);
+}
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status detecting objects";
+    object_name = document.getElementById("object_name").value;
+
+}
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+    status = true;
+}
